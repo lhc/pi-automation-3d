60155732aa37cc0fec2312452bfbc18b263ab22f
juanengml <juanengml@gmail.com>
2024-11-15 20:34:35 +0000

    json

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
-from flask import Flask, render_template, redirect, url_for
-
+from flask import Flask, render_template, redirect, url_for, jsonify
 from lautomation import service, logger
 
 app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    # Obtém o status dos pinos e renderiza no template
-    status = {int(pin.split('_')[1]): state for pin, state in service.get_status().items()}
-    return render_template("index.html", status=status)
-
-@app.route("/toggle/<int:pin>", methods=["POST"])
-def toggle_pin(pin):
-    # Alterna o estado do pino e redireciona para a página inicial
-    current_status = service.get_status().get(f"relay_{pin}")
-    new_action = "off" if current_status == "on" else "on"
-    service.control_pin(pin, new_action)
-    return redirect(url_for("index"))
-
 
 
 @app.route("/api/pin/<int:pin>/on", methods=["GET"])
